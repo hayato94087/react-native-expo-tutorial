a2d2f22b9eac95ce5727887af2ff474b3cc10700
hayato94087 <hayato94087@gmail.com>
2024-09-11 10:25:21 +0900

    エントリーポイントを変更

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,8 +1,9 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
+const App = () => {
+    return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+registerRootComponent(App);
